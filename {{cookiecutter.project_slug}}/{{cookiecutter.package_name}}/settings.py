462ded43c06e1f500989d9412b919af0b437e61a
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2024-04-25 10:50:12 -0300

    Set global static and templates directories.

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +57,7 @@ ROOT_URLCONF = "{{ cookiecutter.package_name }}.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
